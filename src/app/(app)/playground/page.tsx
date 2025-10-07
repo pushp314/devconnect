41bbed16ec4a29c1ff9ca872f3e28b57b48a02b1
PUSHP RAJ SHARMA <kamalabai181@gmail.com>
2025-10-07 18:05:09 +0000

    Try fixing this error: `Unhandled Runtime Error: ReferenceError: React i

--- src/app/(app)/playground/page.tsx
+++ src/app/(app)/playground/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import * as React from 'react';
 import { useState } from 'react';
 import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live';
 import { useTheme } from 'next-themes';
